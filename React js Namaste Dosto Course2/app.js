9f2977402228dfb0f3e4ac0da63a64aecd5319de
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-07-17 14:11:42 +0530

    creating app
    creating react app with professional way

---
+++ React js Namaste Dosto Course2/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div",{ id: "parent" },
+
+    [React.createElement("div", { id: "chaild" }, 
+    React.createElement("h1", { id: "heading2" }, "I am Heading tag"),
+    React.createElement("h2", { id: "h2" }, "I am Heading 2 tag"),
+    )],
+  
+    [React.createElement("div", { id: "chaild2" }, 
+      React.createElement("h1", { id: "heading2" }, "I am Heading tag"),
+      React.createElement("h2", { id: "h2" }, "I am Heading 2 tag"),
+    )],
+  
+  );
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
